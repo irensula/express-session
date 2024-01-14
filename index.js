e85e148da96492473a4985afefa53da76156631f
irensula <irinawebdev@gmail.com>
2024-01-14 10:22:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 
-app.use(session({ secret: 'thisisnotagoodsecret' }));
+app.use(session({ secret: 'thisisnotagoodsecret', resave: 'false', saveUninitialized: false }));
 
 app.get('/viewcount', (req, res) => {
     if(req.session.count) {
@@ -12,6 +12,17 @@ app.get('/viewcount', (req, res) => {
     }
     res.send(`You have viewd this page ${req.session.count} times`);
 });
+
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous'} = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`);
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
